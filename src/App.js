2ea26a7a822c87ed1ca00668b2c0003638eab35f
omgimanoob <41784252+nick@users.noreply.github.com>
2024-07-08 13:50:20 +0800

    completed tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,37 +1,86 @@
-import { useState } from 'react';
+import { useState } from "react";
+
+function Square({ value, onSquareClick, highlight }) {
+  return (
+    <button className={`square ${highlight ? 'highlight' : ''}`}  onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const winnerData = calculateWinner(squares);
+  const winner = winnerData ? winnerData[0] : null;
+  const col1 = winnerData ? winnerData[1] : null;
+  const col2 = winnerData ? winnerData[2] : null;
+  const col3 = winnerData ? winnerData[3] : null;
+  
+
+  function handleClick(i) {
+    if (squares[i] || winner) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
+      <p>
+      {
+        winner ? <>Winner:<strong>{winner}</strong></> : <><strong>{ xIsNext ? "X" : "O" }</strong>'s move</>
+      }
+      </p>
+      
+
+      {
+        [1, 2, 3].map((row) => (
+          <div key={`board-row-${row}`} className="board-row">
+            {
+              [1, 2, 3].map(
+                (col) => {
+                  const index = 3 * (row - 1) + (col - 1);
+                  const highlight = (index === col1 || index === col2 || index === col3);
+                  return (
+                  <Square
+                    highlight = {highlight}
+                    key={`square-${row}-${col}`}
+                    onSquareClick={() => handleClick(index)}
+                    value={squares[index]}
+                  />
+                )}
+              )
+            }
+          </div>
+        ))
+      }
     </>
   );
 }
 
-function Square() {
-  const [value, setValue] = useState(null);
 
-  function handleClick() {
-    setValue("X");
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      console.log(`${squares[a]} has won`)
+      return [squares[a], a, b, c];
+    }
   }
-
-  return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
-  );
+  return [null, null, null, null];
 }
